fe6d16905f9699d9ee087e5fee3bb4cfbdae9041
tschramek <ts@wunschloesung.com>
2023-05-11 11:32:59 +0200

    added cypress and try to get e2e to work

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import localeDe from '@angular/common/locales/de';
+import localeEn from '@angular/common/locales/en';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localeDe);
+registerLocaleData(localeEn);
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
